083be052c488df6aa7a39bac5489ffdce82e06aa
pbr20 <131875727+pbr20@users.noreply.github.com>
2026-06-28 08:04:15 +0000

    set 1

--- set.py
+++ set.py
@@ -6,4 +6,27 @@ print(fruits)
 print(len(fruits))
 
 #checking an item
-print()
+print("IS orange in fruits: ", 'orange' in fruits)
+
+fruits.add('cherry')
+print(fruits)
+
+#update for multiple items to set
+fruits.update(['grape', 'strawberry', 'blueberry'])
+print(fruits)
+
+#remove
+fruits.remove('grape')
+print(fruits)
+
+#removing random items
+
+removed_fruit = fruits.pop()
+print("Removes fruits: ",removed_fruit)
+
+#clearing set
+fruits.clear()
+print(fruits)
+
+
+
