a5e3d332f9f9c9e9a33a8c6d2db4f1aa5de143f2
Shsh <rithen1971@gmail.com>
2026-06-29 21:38:49 +0600

    loop

--- conditionalStatement.py
+++ conditionalStatement.py
@@ -13,3 +13,31 @@ print("Enjoy your day")
 age = 15
 message = "Eligiible to vote" if age >= 18 else "Not eligible to vote"
 print(message)
+
+# practice
+
+person = {
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_married': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+}
+
+if 'skills' in person:
+    skills = person['skills']
+    print("Middle skill: ", skills[len(skills)//2])
+else:
+    print("This person have no skill")
+
+print("Does this person know python",
+      'skills' in person and 'Python' in person['skills'])
+
+if person['is_married'] and 'Finland' in person['country']:
+    print(person['first_name'], person['last_name'],
+          "lives in", person['country'] + ". He is married")
