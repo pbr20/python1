17b1601b3ccfeb3ef06b80aa4a630e08f92f8bde
Shsh <rithen1971@gmail.com>
2026-07-01 16:58:37 +0600

    function

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import math
 
+
 def greet(first_name, last_name):
     print(f"Hello {first_name} {last_name}")
     print("Welcome to the program.")
@@ -42,13 +43,60 @@ def multiply(*numbers):
 print(multiply(1, 2, 3, 4, 5))  # prints 120
 
 
-#finding root using newton method
+# finding root using newton method
 
 def square_root(n):
-    root = n/2 #initial guess
+    root = n/2  # initial guess
 
     for k in range(50):
         root = (1/2) * (root+(n/root))
     return root
 
-print("Root by python: ",math.sqrt(3), "Root by customized function: ",square_root(3))
+
+print("Root by python: ", math.sqrt(3),
+      "Root by customized function: ", square_root(3))
+
+
+# dictionary unpacking
+my_dict = {
+    "first_name": "Rithen",
+    "last_name": "Barua",
+}
+
+
+def greet(first_name, last_name):
+    print(f"Hello {first_name} {last_name}")
+    print("Welcome to the program.")
+
+
+greet(**my_dict)
+
+my_dict1 = {
+    "first_name": "Rithen",
+    "last_name": "Barua",
+    "age": 22,
+    "country": "Bangladesh",
+    "is_student": True,
+    "skills": ["Python", "JavaScript", "C++"]
+}
+
+
+def arbitrary_arguments(**args):
+    for k, v in args.items():
+        print(k, v)
+
+
+arbitrary_arguments(**my_dict1)
+
+# Understanding * vs **
+# * unpacks the keys of the dictionary.
+# ** unpacks the values of the dictionary.
+
+# Function as a Parameter of Another Function
+
+
+def do_smth(func, x):
+    return func(x)
+
+
+print(do_smth(square_root, 9))  # prints 3.0
